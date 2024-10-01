f09ef0442b1220b73ebcb43832c97100922b52aa
vanji <166405995+vanji123@users.noreply.github.com>
2024-10-01 16:34:38 +0530

    Update listing aws resources.sh

--- listing aws resources.sh
+++ listing aws resources.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
-
-###############################################################################
-# Author: Abhishek Veeramalla
-# Version: v0.0.1
-
 # Script to automate the process of listing all the resources in an AWS account
-#
-# Below are the services that are supported by this script:
-# 1. EC2
-# 2. RDS
-# 3. S3
-# 4. CloudFront
-# 5. VPC
-# 6. IAM
-# 7. Route53
-# 8. CloudWatch
-# 9. CloudFormation
-# 10. Lambda
-# 11. SNS
-# 12. SQS
-# 13. DynamoDB
-# 14. VPC
-# 15. EBS
-#
-# The script will prompt the user to enter the AWS region and the service for which the resources need to be listed.
-#
-# Usage: ./aws_resource_list.sh  <aws_region> <aws_service>
-# Example: ./aws_resource_list.sh us-east-1 ec2
-#############################################################################
 
 # Check if the required number of arguments are passed
 if [ $# -ne 2 ]; then
